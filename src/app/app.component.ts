ad72fc10825afef0a716e667e5d99ed41d19ac9f
DouglasIde <douglasymide@gmail.com>
2024-08-27 16:40:26 -0300

    Feat: Loading data with OnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Item } from './interfaces/iItem';
+import { Component, OnInit } from '@angular/core';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app-lista-de-compras';
 
-  constructor() { }
+export class AppComponent implements OnInit{
+  title = 'app-lista-de-compras';
+  ListaDeCompra! : Array<Item>
+
+  constructor(private listaService: ListaDeCompraService) { }
+
+  ngOnInit(): void{
+    this.ListaDeCompra = this.listaService.getListaDeCompra();
+    console.log(this.ListaDeCompra);
+    
+  }
 }
